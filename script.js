9b50d3bfcde614a9cfabe8201621cf5f32771a1f
Shubham Innani <2016bec035@sggs.ac.in>
2023-01-03 20:39:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Deep Learning Enthusiast", "Medical Vision Researcher"],
+        strings: ["Deep Learning Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Deep Learning Enthusiast", "Medical Vision Researcher"],
+        strings: ["Deep Learning Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
